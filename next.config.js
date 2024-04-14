46dfb4babc3099bb48a4f4a21ac9a93fa7d3f8c6
Graham McBain <grahammcbain@gmail.com>
2024-04-14 10:33:23 -0700

    CSP update

--- next.config.js
+++ next.config.js
@@ -6,8 +6,7 @@ module.exports = {
 	webpack(config) {
 		config.resolve.alias = {
 			...config.resolve.alias,
-			// This is a hammer approach for local dev. We can't figure out why, but node 18 starts to have
-			// issues with local dev seeing multiple Reacts.
+			// Resolving potential issues with multiple versions of React in development
 			react: path.resolve('./node_modules/react'),
 		}
 		return config
@@ -20,14 +19,48 @@ module.exports = {
 				headers: [
 					{
 						key: 'Content-Security-Policy',
+						// Adjust CSP based on development or production environment
 						value:
-							"default-src 'self'; script-src 'self' 'https://auth.privy.io'; img-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' 'https://auth.privy.io';",
+							process.env.NODE_ENV === 'development'
+								? "default-src 'self'; script-src 'self' 'https://auth.privy.io' 'unsafe-eval'; img-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' 'https://auth.privy.io'; connect-src 'self' 'https://auth.privy.io';"
+								: "default-src 'self'; script-src 'self' 'https://auth.privy.io'; img-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' 'https://auth.privy.io'; connect-src 'self' 'https://auth.privy.io';",
 					},
 				],
 			},
 		]
 	},
 }
+
+// const path = require('path')
+
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+// 	reactStrictMode: true,
+// 	webpack(config) {
+// 		config.resolve.alias = {
+// 			...config.resolve.alias,
+// 			// This is a hammer approach for local dev. We can't figure out why, but node 18 starts to have
+// 			// issues with local dev seeing multiple Reacts.
+// 			react: path.resolve('./node_modules/react'),
+// 		}
+// 		return config
+// 	},
+// 	async headers() {
+// 		return [
+// 			{
+// 				// Apply these headers to all routes in your application.
+// 				source: '/:path*',
+// 				headers: [
+// 					{
+// 						key: 'Content-Security-Policy',
+// 						value:
+// 							"default-src 'self'; script-src 'self' 'https://auth.privy.io'; img-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' 'https://auth.privy.io';",
+// 					},
+// 				],
+// 			},
+// 		]
+// 	},
+// }
 
 // const path = require('path');
 
