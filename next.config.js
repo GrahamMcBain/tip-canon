39a7b8d72edb27b54b109888010a295435a18a8e
Graham McBain <grahammcbain@gmail.com>
2024-04-14 10:22:09 -0700

    CSP fix?

--- next.config.js
+++ next.config.js
@@ -1,15 +1,46 @@
-const path = require('path');
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // This is hammer approach for local dev. We can't figure out why, but node 18 starts to have
-      // issues with local dev seeing multiple reacts.
-      react: path.resolve('./node_modules/react'),
-    };
-    return config;
-  },
-};
+	reactStrictMode: true,
+	webpack(config) {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			// This is a hammer approach for local dev. We can't figure out why, but node 18 starts to have
+			// issues with local dev seeing multiple Reacts.
+			react: path.resolve('./node_modules/react'),
+		}
+		return config
+	},
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: '/:path*',
+				headers: [
+					{
+						key: 'Content-Security-Policy',
+						value:
+							"default-src 'self'; script-src 'self' 'https://auth.privy.io'; img-src 'self'; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' 'https://auth.privy.io';",
+					},
+				],
+			},
+		]
+	},
+}
+
+// const path = require('path');
+
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+//   webpack(config) {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       // This is hammer approach for local dev. We can't figure out why, but node 18 starts to have
+//       // issues with local dev seeing multiple reacts.
+//       react: path.resolve('./node_modules/react'),
+//     };
+//     return config;
+//   },
+// };
