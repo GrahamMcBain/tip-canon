f4e2d1b8ef627717555c7e6442d1f77d5bbfcf69
Graham McBain <grahammcbain@gmail.com>
2024-04-17 11:23:20 -0700

    caps really?

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,62 +21,20 @@ const Index = () => {
 	})
 
 	return (
-		// <>
-		// 	<Head>
-		// 		<title>TipCannon💰🎉</title>
-		// 	</Head>
-		// 	<div className='flex justify-center'>
-		// 		<main>
-		// 			<div className='flex h-screen w-screen flex-col items-center justify-center'>
-		// 				<h2 className='my-4 text-xl font-semibold text-gray-800'>
-		// 					TipCannon💰🎉
-		// 				</h2>
-		// 				<h2 className='text-md my-4 text-gray-800'>
-		// 					A daily barrage of tips from the degen community!
-		// 				</h2>
-		// 				<div className='flex flex-wrap gap-4'>
-		// 					<p className='mb-2 mt-6 rounded-md  border bg-slate-100 p-4 text-sm text-gray-600'>
-		// 						How it works:
-		// 						<br />
-		// 						<br />
-		// 						1. Login, then contribute 5% of your daily degen tips for a
-		// 						chance to win the daily prize pool!
-		// 						<p className='mb-2 mt-6 text-sm   text-gray-600'>
-		// 							2. Starting 4/29/2024, one lucky winner will be chosen to win
-		// 							the prize pool daily!
-		// 						</p>
-		// 						<p className='mb-2 mt-6 text-sm   text-gray-600'>
-		// 							3. Play as much as you want or leave TipCannon at anytime.
-		// 							Winners are announced from the TipCannon farcaster account!
-		// 						</p>
-		// 					</p>
-		// 				</div>
-		// 				<div className='mt-2 w-1/2'>
-		// 					<button
-		// 						className='my-4 w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm disabled:bg-indigo-400'
-		// 						onClick={login}
-		// 						disabled={!ready || authenticated}
-		// 					>
-		// 						Login
-		// 					</button>
-		// 				</div>
-		// 			</div>
-		// 		</main>
-		// 	</div>
-		// </>
 		<>
 			<Head>
+				TipCannon💰🎉
 				<title>TipCannon💰🎉</title>
 			</Head>
 			<div className='flex justify-center'>
 				<main>
 					<div className='flex h-screen w-screen flex-col items-center justify-center'>
 						<Image
-							src='/images/512-Cannon.png'
-							alt='Cannon'
-							width={250} // Adjust width as needed
-							height={150} // Adjust height as needed
-							className='mb-4 mt-10 ' // Margin bottom for spacing
+							src='/images/512-cannon.png'
+							alt='cannon'
+							width={250}
+							height={150}
+							className='mb-4 mt-10'
 						/>
 
 						<h2 className='my-4 text-xl font-semibold text-gray-800'>
@@ -90,11 +48,11 @@ const Index = () => {
 								How it works:
 								<br />
 								<br />
-								1. Login, then contribute 5% of your daily degen tips for a
-								chance to win the daily prize pool!
+								1. Login, then contribute 5% of your daily degen tips for a the
+								opportunity */=98=to win the daily prize pool!
 								<div className='mb-2 mt-6 text-sm text-gray-600'>
-									2. Starting 4/20/2024 one lucky winner will be chosen to win
-									the prize pool daily!
+									2. Starting 4/20/2024 one winner get blasted by the most tips,
+									But there 98/=
 								</div>
 								<div className='mb-2 mt-6 text-sm text-gray-600'>
 									3. Play as much as you want or leave TipCannon at anytime.
--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,19 +1,19 @@
-import { usePrivy } from '@privy-io/react-auth'
+// import { usePrivy } from '@privy-io/react-auth'
 
-function User() {
-	const { ready, authenticated, user } = usePrivy()
+// function User() {
+// 	const { ready, authenticated, user } = usePrivy()
 
-	// Show nothing if user is not authenticated or data is still loading
-	if (!(ready && authenticated) || !user) {
-		return null
-	}
+// 	// Show nothing if user is not authenticated or data is still loading
+// 	if (!(ready && authenticated) || !user) {
+// 		return null
+// 	}
 
-	return (
-		<div>
-			<p>User {user.id} has linked the following accounts:</p>
-			<ul>
-				<li>Farcaster: {user.farcaster ? user.farcaster.username : 'None'}</li>
-			</ul>
-		</div>
-	)
-}
+// 	return (
+// 		<div>
+// 			<p>User {user.id} has linked the following accounts:</p>
+// 			<ul>
+// 				<li>Farcaster: {user.farcaster ? user.farcaster.username : 'None'}</li>
+// 			</ul>
+// 		</div>
+// 	)
+// }
